e95099e8e4f70ee16dae9bd7151c6ec9703ed9a6
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-09-06 12:03:48 +0100

    chore: rename controllers package (#37)

--- internal/controllers/gitrepository_predicate.go
+++ internal/controllers/gitrepository_predicate.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package controller
+package controllers
 
 import (
 	"sigs.k8s.io/controller-runtime/pkg/event"
